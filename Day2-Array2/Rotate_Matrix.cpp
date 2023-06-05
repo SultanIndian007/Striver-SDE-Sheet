9a686b9ca320d2ff8efefd8d55b49f5e24464f3b
SultanIndian007 <amantiwari201608@gmail.com>
2023-06-05 20:16:14 +0530

    5qns

---
+++ Day2-Array2/Rotate_Matrix.cpp
@@ -0,0 +1,16 @@
+// TC -> O(n^2) SC -> O(n) in place , Transpose then reverse
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<i; j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for(int i=0; i<m; i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
